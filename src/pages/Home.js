5dd640bf3b2cc47107b4cfe539c4b270d53d1617
allanstar-byte <allanmutisya263@gmail.com>
2022-07-28 11:48:28 +0300

    Home

--- src/pages/Home.js
+++ src/pages/Home.js
@@ -4,17 +4,24 @@ import BannerImage from "../assets/pizza.jpeg";
 import "../styles/Home.css";
 
 function Home() {
-  return (
-    <div className="home" style={{ backgroundImage: `url(${BannerImage})` }}>
-      <div className="headerContainer">
-        <h1> Pedro's Pizzeria </h1>
-        <p> PIZZA TO FIT ANY TASTE</p>
-        <Link to="/menu">
-          <button> ORDER NOW </button>
-        </Link>
-      </div>
-    </div>
-  );
+    return ( <
+        div className = "home"
+        style = {
+            { backgroundImage: `url(${BannerImage})` }
+        } >
+        <
+        div className = "headerContainer" >
+        <
+        h1 > Breathtaking, Splendid, Wondrous < /h1> <
+        p > Tulia gives it all < /p> <
+        Link to = "/menu" >
+        <
+        button >
+        Let 's Roll < /button> < /
+        Link > <
+        /div> < /
+        div >
+    );
 }
 
 export default Home;
