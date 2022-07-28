88e8504a08faf73a6f7c913a44017214c1ccfc28
allanstar-byte <allanmutisya263@gmail.com>
2022-07-28 16:56:39 +0300

    Menu

--- src/helpers/MenuList.js
+++ src/helpers/MenuList.js
@@ -1,39 +1,39 @@
-import Pepperoni from "../assets/pepperoni.jpg";
-import Margherita from "../assets/margherita.jpg";
-import PedroTechSpecial from "../assets/pedrotechspecial.jpg";
-import Vegan from "../assets/vegan.jpg";
-import Pineapple from "../assets/pineapple.jpg";
-import Expensive from "../assets/expensive.jpg";
+import Peponi from "../assets/Peponi.jpg";
+import Mpembe from "../assets/Mpembe.jpg";
+import Wakanda from "../assets/wakanda.jpeg";
+import Kilemoni from "../assets/kilemoni.jpeg";
+import Uzuri from "../assets/Uzuri.jpeg";
+import Majengo from "../assets/Majengo.jpg";
 
-export const MenuList = [
-  {
-    name: "Pepperoni Pizza",
-    image: Pepperoni,
-    price: 15.99,
-  },
-  {
-    name: "Margherita Pizza",
-    image: Margherita,
-    price: 11.99,
-  },
-  {
-    name: "PedroTech Special Pizza",
-    image: PedroTechSpecial,
-    price: 256.53,
-  },
-  {
-    name: "Vegan Pizza",
-    image: Vegan,
-    price: 17.99,
-  },
-  {
-    name: "Pineapple Pizza",
-    image: Pineapple,
-    price: 4.99,
-  },
-  {
-    name: "Very Expensive Pizza",
-    image: Expensive,
-    price: 1997.99,
-  },
+export const MenuList = [{
+        name: "Peponi House",
+        image: Peponi,
+        price: 45999.99,
+
+    },
+    {
+        name: "Mpembe Grounds",
+        image: Mpembe,
+        price: 50000.00,
+    },
+    {
+        name: "Wakanda Homes",
+        image: Wakanda,
+        price: 33256.53,
+    },
+    {
+        name: "Kilemoni Homes",
+        image: Kilemoni,
+        price: 44417.99,
+    },
+    {
+        name: "Uzuri Wote",
+        image: Uzuri,
+        price: 55554.99,
+    },
+    {
+        name: "Majengo Mapya",
+        image: Majengo,
+        price: 61997.99,
+    },
 ];
